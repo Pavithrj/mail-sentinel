066ca082b2fa19c37c5ef5f633064ef87ec35f00
Pavithr Jain <pavithrjain19@gmail.com>
2025-12-03 21:56:14 +0530

    022

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, getAllUsers, getAllAdmins, deleteUser, deleteAdmin } = require('../controllers/authController');
 const router = express.Router();
 
 router.post("/register", register);
-
 router.post("/login", login);
+
+router.get("/users", getAllUsers);
+router.get("/admins", getAllAdmins);
+
+router.delete("/user/:id", deleteUser);
+router.delete("/admin/:id", deleteAdmin);
 
 module.exports = router;
