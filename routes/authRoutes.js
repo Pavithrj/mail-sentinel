2c76f7f0a641dd0d3321c18a870fdb83b8e1ff0e
Pavithr Jain <pavithrjain19@gmail.com>
2025-12-03 18:07:09 +0530

    009

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post("/register", registerUser);
 
-router.post('/login', loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
