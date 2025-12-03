28263b5601dbe921ed16c05c5225cf6de822c90c
Pavithr Jain <pavithrjain19@gmail.com>
2025-12-03 18:07:23 +0530

    010

--- models/User.js
+++ models/User.js
@@ -21,11 +21,11 @@ const userSchema = new mongoose.Schema(
         },
         role: {
             type: String,
-            enum: ['user', 'admin'],
-            default: 'user'
+            enum: ["user", "admin"],
+            default: "user"
         }
     },
     { timestamps: true }
 );
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
