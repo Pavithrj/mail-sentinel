1c7300bf4f4ac9ca760d110bc9c1c084c9faa32b
Pavithr Jain <pavithrjain19@gmail.com>
2025-12-03 18:08:13 +0530

    012

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
